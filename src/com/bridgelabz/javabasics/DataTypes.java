1525003b19ce48ac227439c2289b0d76809b2b3a
PREETI2096 <108446384+PREETI2096@users.noreply.github.com>
2023-01-10 13:54:50 +0530

    Added : basic java programs

--- src/com/bridgelabz/javabasics/DataTypes.java
+++ src/com/bridgelabz/javabasics/DataTypes.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class DataTypes {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner (System.in);
+	    Scanner scanner = new Scanner (System.in);
 		
 	    System.out.println("Enter whole number :");
 	    byte n = scanner.nextByte();
